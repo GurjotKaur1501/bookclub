38fb2ffc73fbfae017db7ccaa80f49b68fa5a5c5
GurjotKaur1501 <jotkau0115@gmail.com>
2025-11-11 09:15:57 +0100

    Update Book.java

--- src/main/java/se/yrgo/bookclub/domain/Book.java
+++ src/main/java/se/yrgo/bookclub/domain/Book.java
@@ -1,4 +1,25 @@
 package se.yrgo.bookclub.domain;
 
 public class Book {
+    private String title;
+    private String author;
+    private String genre;
+
+    public Book() {}
+
+    public Book(String title, String author, String genre) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+    }
+
+    // getters and setters
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getAuthor() { return author; }
+    public void setAuthor(String author) { this.author = author; }
+
+    public String getGenre() { return genre; }
+    public void setGenre(String genre) { this.genre = genre; }
 }
